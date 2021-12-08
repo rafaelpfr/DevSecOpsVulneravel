f3903cd8b48452e3af7635330e57234c40a1648d
rafaelpfr <74310472+rafaelpfr@users.noreply.github.com>
2021-12-08 18:50:51 -0300

    Delete verifyRedirect.js

--- middlewares/verifyRedirect.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res, next){
-  let validPaths = ['/audience', '/speakers']
-
-  if (req.query.redirect_url && validPaths.indexOf(req.query.redirect_url) === -1) 
-    return res.status(404).send('Invalid Path');
-
-    next();
-}
